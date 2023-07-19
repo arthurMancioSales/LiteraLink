51af4318b953cb1147db1bf0b1c868d9c5105a32
Anderson L <113462710+andersonlsouza@users.noreply.github.com>
2023-07-18 22:42:03 -0300

    Modelo básico das pastas

---
+++ app/(public)/sign-in/page.tsx
@@ -0,0 +1,5 @@
+export default function SignPage() {
+    return (
+        <>Cadastro</>
+    );
+}
---
+++ src/constants/appRoutes.ts
@@ -0,0 +1,14 @@
+export const APP_ROUTES = {
+    private: {
+        dashboard: {
+            name: "/dashboard"
+        },
+        unauthorized: {
+            name: "unauthorized"
+        }
+    },
+    public: {
+        login: "/",
+        forgetPassword: "/forget-password"
+    }
+};
