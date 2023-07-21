356d479c5ac540226a40f16cea131de0ec5039a0
Anderson L <113462710+andersonlsouza@users.noreply.github.com>
2023-07-20 23:57:09 -0300

    feat: Implantação de uma biblioteca de icones
    #13

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,5 +1,19 @@
+"use client";
+
+// import { Progress } from "@chakra-ui/react";
+import { AiFillApple } from "react-icons/ai";
+
 export default function Home() {
     return (
-        <>Home</>
+        <>
+            Home
+            
+            {/* <Progress value={50}/> */}
+            <button className="bg-gray-800 hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center">
+                <AiFillApple/>
+                <span>Download</span>
+            </button>
+        </>
+        // <>Home</>
     );
 }
