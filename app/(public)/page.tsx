2e2798cac5d634a655b011a9f87412c61679a670
Anderson L <113462710+andersonlsouza@users.noreply.github.com>
2023-07-22 18:13:20 -0300

    fix: A página não estava sendo renderizada
    Foi colocado o termo "use client na página por causa do onclick do botão"
    
    #13

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,8 +1,10 @@
+"use client";
+
 import { AiFillApple } from "react-icons/ai";
 import { Button } from "@/src/components/Button";
 
 export default function Home() {
     return (
-        <Button icon={<AiFillApple size={25}/>} onClick={() => console.log()}>Teste</Button>
+        <Button icon={<AiFillApple size={25}/>} onClick={() => console.log("Clicou")}>Teste</Button>
     );
 }
