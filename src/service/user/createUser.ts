01f54f05b9b6b22d8c32179390b1a4d5db3f130f
seitih <henrique.seiti1@gmail.com>
2023-07-26 14:43:10 -0300

    feat: routes book-list (DELETE*, POST) new-user, community

--- src/service/user/createUser.ts
+++ src/service/user/createUser.ts
@@ -1,10 +1,10 @@
 import { users } from "../../repository/users";
+import { insertUser } from "../../repository/users";
 import { CustomError } from "../../utils/customError";
-import { IUser } from "@/src/interfaces/interface";
+import { INewUser, IUser } from "@/src/interfaces/interface";
 
 export async function registerUser( requestUser: IUser ) {
-    // refatorar, ta errado!
-    // try {
+    // refatorar, ta errado!   
     const user = users.find(_user => _user.email === requestUser.email);
     const id = users.find(_user => _user._id === requestUser.id); // temporario
     if(user) {
@@ -13,7 +13,28 @@ export async function registerUser( requestUser: IUser ) {
     if(id) {
         throw new CustomError("Error: id já está em uso.", 400);
     }
-    // } catch (error: any) {
-    //     throw error;
-    // }
+    if(requestUser.name === undefined || requestUser.email === undefined || requestUser.password === undefined) {
+        throw new CustomError("Error: missing information.", 400);
+    }
+    else {
+        // const newUser : INewUser = {
+        //     name: requestUser.name,
+        //     email: requestUser.email,
+        //     password: requestUser.password,
+        //     image: "",
+        //     communities: [],
+        //     books: [],
+        //     statistics: {
+        //         lastSequence: new Date,
+        //         readingTime: 0,
+        //         maxSequence: 0,
+        //         actualSequence: 0,
+        //         goalsAchieved: 0,
+        //     },
+        // };
+
+        const insertedUser = insertUser(requestUser);
+        console.log(insertedUser);
+    }
+   
 }
