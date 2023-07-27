d777456229c5217a0addf440b9af051ffb2fbcff
Anderson L <113462710+andersonlsouza@users.noreply.github.com>
2023-07-26 23:18:41 -0300

    feat: Página de Comunidade atualizada
    Parte visual está quase finalizada, faltando apenas estilizar os botões e pegar as requisições
    
    #42

--- src/components/CardCommunity/index.tsx
+++ src/components/CardCommunity/index.tsx
@@ -1,29 +1,39 @@
-import { BgCardCommunity } from "../BgCardCommunity";
+// import { BgCardCommunity } from "./BgCardCommunity";
 import { Button } from "../Button";
+import Image from "next/image";
 
 type PropsTypes = {
-    imageUser: string;
-    idcommunity: string;
-    generocommunity: string;
-
+    community: {
+        id: number;
+        name: string;
+        image: string;
+        auth: string;
+        category: string;
+        gender: string;
+    }
 };
 
-export function CardCommunity({imageUser, idcommunity, generocommunity}: PropsTypes) { 
+export function CardCommunity({community}: PropsTypes) {
     function handleDashBoard() {
         console.log("clicou");                 
     }
     return (
-        <div className="flex flex-col w-1/5 space-y-4 m-3 pb-2 items-start rounded-lg bg-primaryLight dark:bg-secondaryDar">
-            <BgCardCommunity src={imageUser} size={180}/>
-
-            <p className="pl-4">{idcommunity}</p>
-            
-            <div className="flex pl-4 space-x-3 text-xs">
-                <p>{"#"+generocommunity}</p>
-                <p>{"#"+generocommunity}</p>
+        <div className="overflow-hidden rounded-lg relative w-[200px] h-[250px] bg-light-primary">
+            <div className="h-[95px] relative">
+                <Image
+                    className="object-cover"
+                    src={community.image}
+                    alt=""
+                    fill
+                />
             </div>
-
-            <div className="flex pl-2.5 bg-#8B5CF6">
+            <div className="flex flex-col justify-between content-between px-2 pb-2">
+                <div>
+                    <p className="text-2xl font-bold">{community.name}</p>
+                    <p><span className="font-bold">Autor: </span>{community.auth}</p>
+                    <p><span className="font-bold">Categoria: </span>{community.category}</p>
+                    <p><span className="font-bold">Gênero </span>{community.gender}</p>
+                </div>
                 <Button onClick={handleDashBoard}>Voltar</Button>
             </div>
         </div>
