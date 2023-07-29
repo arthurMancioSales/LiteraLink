66ed9a912048b0fddd9bcb9d50b5ba15f3ab617d
Anderson L <113462710+andersonlsouza@users.noreply.github.com>
2023-07-28 22:59:10 -0300

    feat: Desenvolvimento inicial da página da home
    #53

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,21 +1,12 @@
 "use client";
 
 import { Button } from "@/src/components/Button";
+import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
-
-
-import { UserGoals } from "@/src/components/UserGoals";
-import { AiFillAndroid } from "react-icons/ai";
-import { SearchForm } from "@/src/components/SearchBar";
 
 export default function Home() {
     const [auth, setAuth] = useState(false);
-    const [searchValue, setSearchValue] = useState("");
-
-    const handleSearch = (value: string) => {
-        setSearchValue(value);
-        console.log("Realizar a busca com o valor:", value);
-    };
     
     async function apiFetch() {
         const req = await fetch("/api/login", {
@@ -38,18 +29,26 @@ export default function Home() {
     }
 
     return (
-        <>
-            <p>home</p>
+        <div className="flex flex-col h-screen">
+            <div className="flex justify-between items-center px-20 bg-light-tertiary h-[62px]">
+                <Link href="/">LiteraLink</Link>
+                <div className="flex gap-2">
+                    <Link href="/dashboard">Entrar</Link>
+                    <div>Não sei</div>
+                </div>
+            </div>
+            <div className="flex justify-around items-center h-full px-20 bg-light-secondary">
+                <div>
+                    <p className="text-7xl mb-4">Título</p>
+                    <p className="text-justify">Textkfjsdkfjsdkfj sdjfksddfsdfdsfsdfsd fdsfsdfdsfdsfs dfsdfdssf gdsgdfgghfg hfghfghfghf ghfghgf hfghf jdkajsdkjasd dsjkdaskdja kdkasjdkajdkaj dksajdkasjdkasjd askdasjkdsajdkasj kdjask djask jdaskjdkasjdakslorem50</p>
+                </div>
+                <div className="h-[400px] w-[400px] relative">
+                    <Image className="object-contain" src="/images/home/index.png" alt="" fill priority/>
+                </div>
+            </div>
             {auth ? <p>Logado</p> : <p>Não logado</p> }
             <Button onClick={apiFetch}>Login</Button>
             <Button redirectTo="/dashboard">Dashboard</Button>
-
-            <UserGoals
-                icon={<AiFillAndroid size={50}/>}
-                value={99}>
-                    Maior sequência
-            </UserGoals>
-            <SearchForm onSearch={handleSearch} value={searchValue}/>
-        </>
+        </div>
     );
 }
