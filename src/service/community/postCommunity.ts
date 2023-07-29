56d00feda37f592ae70f34fe3650dbcefb07f316
Eduardo Henrique Silva Santos <eduardo.h3nriqu3@gmail.com>
2023-07-29 12:59:14 -0300

    fix(correção): Correções necessárias do lint

--- src/service/community/postCommunity.ts
+++ src/service/community/postCommunity.ts
@@ -4,28 +4,35 @@ import { createCommunity } from "@/src/repository/community/createCommunityRepo"
 import { CustomError } from "@/src/utils/customError";
 
 export async function postCommunity(user: {id: string, name: string}, request: ICreateCommunity) {
-    try {
-        const body = {
-            _id: new ObjectId(),
-            name: request.name,
-            description: request.description,
-            favoriteBook: "",
-            image: "",
-            is_admin: user.id,
-            members: [
-                {
-                    id: user.id,
-                    name: user.name
-                }
-            ]
-        };
-        const response = await createCommunity(body);
-        if (!response) {
-            throw new CustomError("Erro na criação da comunidade", 500);
-        }
-        delete response._id;
-        return response;
-    } catch (e: any) {
-        throw e;
+    const body = {
+        _id: new ObjectId(),
+        name: request.name,
+        description: request.description,
+        favoriteBook: "",
+        image: "",
+        is_admin: user.id,
+        members: [
+            {
+                id: user.id,
+                name: user.name
+            }
+        ]
+    };
+    const response = await createCommunity(body);
+    if (!response) {
+        throw new CustomError("Erro na criação da comunidade", 500);
     }
+    return reponseCommunity(response);
+}
+
+function reponseCommunity(community: any) {
+    const response = {
+        name: community.name,
+        description: community.description,
+        favoriteBook: community.favoriteBook,
+        image: community.image,
+        is_admin: community.is_admin,
+        members: community.members
+    };
+    return response;
 }
