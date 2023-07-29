20be0da25a978717e051215d26453f5128c8a31c
Anderson L <113462710+andersonlsouza@users.noreply.github.com>
2023-07-29 14:05:47 -0300

    feat: Pagina de Home
    Página de home quase concluída
    
    #53

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/src/components/Button";
+import { BsFillPersonFill } from "react-icons/bs";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
@@ -30,20 +31,20 @@ export default function Home() {
 
     return (
         <div className="flex flex-col h-screen">
-            <div className="flex justify-between items-center px-20 bg-light-tertiary h-[62px]">
+            <div className="flex justify-between items-center px-20 py-2 bg-light-tertiary h-[62px]">
                 <Link href="/">LiteraLink</Link>
-                <div className="flex gap-2">
-                    <Link href="/dashboard">Entrar</Link>
-                    <div>Não sei</div>
+                <div className="flex gap-2 items-center">
+                    <Button redirectTo="/sign-in" icon={<BsFillPersonFill size={15}/>} variant="secondary">Entrar</Button>
+                    <Button redirectTo="/sign-up" variant="info">Cadastrar</Button>
                 </div>
             </div>
-            <div className="flex justify-around items-center h-full px-20 bg-light-secondary">
-                <div>
+            <div className="flex justify-around items-center h-full px-20 bg-home">
+                <div className="w-[400px]">
                     <p className="text-7xl mb-4">Título</p>
                     <p className="text-justify">Textkfjsdkfjsdkfj sdjfksddfsdfdsfsdfsd fdsfsdfdsfdsfs dfsdfdssf gdsgdfgghfg hfghfghfghf ghfghgf hfghf jdkajsdkjasd dsjkdaskdja kdkasjdkajdkaj dksajdkasjdkasjd askdasjkdsajdkasj kdjask djask jdaskjdkasjdakslorem50</p>
                 </div>
                 <div className="h-[400px] w-[400px] relative">
-                    <Image className="object-contain" src="/images/home/index.png" alt="" fill priority/>
+                    <Image className="object-contain" src="/images/home/imageHome.png" alt="" fill priority/>
                 </div>
             </div>
             {auth ? <p>Logado</p> : <p>Não logado</p> }
