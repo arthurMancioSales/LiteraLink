6b42bbbf48f89e2f4c5caec984195a72ca76aca3
Eduardo Henrique Silva Santos <eduardo.h3nriqu3@gmail.com>
2023-08-06 13:51:18 -0300

    fix(correção): Correção do conflito

--- src/repository/community/getCommunityByNameRepo.ts
+++ src/repository/community/getCommunityByNameRepo.ts
@@ -1,12 +1,8 @@
 import { createMongoConnection } from "@/src/database/pool";
 
-<<<<<<< HEAD:src/repository/community/getCommunityByIdRepo.ts
 const TAG = "REPOSITORY(GET): community ";
 
-export async function getCommunityByIdRepo(community_name : string) {
-=======
 export async function getCommunityByNameRepo(community_name : string) {
->>>>>>> b341426de36d5cd52c22c49dadce392e2c3c9f32:src/repository/community/getCommunityByNameRepo.ts
     const dbConnect = createMongoConnection();
     const client = await dbConnect.connect();
     const collection = client.db("literalink-dev").collection("community"); 
