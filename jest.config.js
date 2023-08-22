fa545ab74e553dd92d47aaab05d799003ffe5bba
Arthur Mancio Sales <arthur_mancio@outlook.com>
2023-08-21 23:17:53 -0300

    Co-authored-by: rickEDU <rickEDU@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
@@ -6,5 +7,22 @@ module.exports = {
         "^@/(.*)$": "<rootDir>/$1",
         '^@/public/images/user/default_user_image.jpg$': '<rootDir>/test_variables/imageMock.js',
     },
-    setupFiles: ["<rootDir>/src/utils/setup-tests.ts"]
+    setupFiles: ["<rootDir>/src/utils/setup-tests.ts"],
+    transform: {
+        "^.+\\.(ts|tsx)$": "ts-jest",
+        "^.+\\.(js)$": "babel-jest",
+    },
+    transformIgnorePatterns: [
+        'node_modules/(?!' + 
+            [
+                'node-fetch',
+                'next/types',
+                'supertest',
+                'fetch-blob',
+                'data-uri-to-buffer',
+                'jest-runtime',
+                'formdata-polyfill'
+            ].join('|') +
+        ')',
+    ],
 };
