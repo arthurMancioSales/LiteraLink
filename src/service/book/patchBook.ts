77452a503070c05e089ff01c89266d94e340b9ec
Eduardo Henrique Silva Santos <eduardo.h3nriqu3@gmail.com>
2023-08-22 00:53:11 -0300

    fix(correção): correção na atualização do livro, de 'lendo' para 'lido'

--- src/service/book/patchBook.ts
+++ src/service/book/patchBook.ts
@@ -18,12 +18,14 @@ export async function patchBook(id: string , body: IPatchBook) {
             if (!oldBook) {
                 throw new CustomError("Livro não enccontrado.", 404);
             }
-            requestBodyRepo.pagesRead += oldBook.pagesRead;
-            if (requestBodyRepo.pagesRead! >= oldBook.totalPages) {
-                requestBodyRepo.status = "lido";
-            }
             if (requestBodyRepo.pagesRead! > 0 && body.pagesRead! > 0) {
                 requestBodyRepo.status = 'lendo';
+            }
+            requestBodyRepo.pagesRead += oldBook.pagesRead;
+            console.log(requestBodyRepo.pagesRead! >= oldBook.totalPages, 'verificacao')
+            if (requestBodyRepo.pagesRead! >= oldBook.totalPages) {
+                console.log('entrou')
+                requestBodyRepo.status = 'lido';
             }
             // garante que o número de páginas lidas é igual ao número total de páginas do livro
             if (requestBodyRepo.pagesRead! > oldBook.totalPages) {
@@ -41,7 +43,9 @@ export async function patchBook(id: string , body: IPatchBook) {
                 throw new CustomError("Erro na atualização da Meta", 500);
             }
             if (body.status) {
-                delete requestBodyRepo.status;
+                if(body.status !== 'lido'){
+                    delete requestBodyRepo.status;
+                }
             }
         }
         const responseDB = await updateBookRepo(userObjectId, body.id, requestBodyRepo);
