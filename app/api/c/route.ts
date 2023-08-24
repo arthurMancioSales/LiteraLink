0316b765ef6e3149c513763bf7a327f38dbbb0ea
seitih <henrique.seiti1@gmail.com>
2023-08-24 20:39:50 -0300

    remove redis for cached all communities

--- app/api/c/route.ts
+++ app/api/c/route.ts
@@ -1,13 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import { createResponse } from "@/src/utils/response";
 import { getAllCommunities } from "@/src/service/community/getAllCommunities";
-import { CustomError } from "@/src/utils/customError";
+
 
 export async function GET(req:NextRequest) {
     const Response = createResponse();
     try {
         const dbResponse = await getAllCommunities();
-        
         Response.data = dbResponse;
         return NextResponse.json(Response, {status: Response.status});
         
