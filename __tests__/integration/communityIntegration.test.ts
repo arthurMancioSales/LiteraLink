8dc3dac7c1e40cab4b7cd74c1604c6ac40993da9
Eduardo Henrique Silva Santos <eduardo.h3nriqu3@gmail.com>
2023-08-24 19:28:57 -0300

    feat(test): teste da rota /api/c/[comunidade]

--- __tests__/integration/communityIntegration.test.ts
+++ __tests__/integration/communityIntegration.test.ts
@@ -4,18 +4,36 @@ import { createCommunityForTest } from "../../src/utils/test_util/createCommunit
 import { createCookie } from "../../src/utils/test_util/createCookie";
 import { createUserForTest } from "../../src/utils/test_util/createUserForTest";
 
-describe('teste das rotas de community', () =>{
-    it('teste na rota /api/c', async () =>{
+describe('teste da rota /api/c', () =>{
+    it('Deve retornar 404 casó não tenha nenhuma comunidade cadastrada', async () =>{
         const res = await request('http://web-test:6060')
         .get(`/api/c`);
         expect(res.status).toEqual(404);
     });
 
-    it('teste na rota /api/c', async () =>{
+    it('Deve retornar 200 casó tenha pelo menos 1 comunidade cadastrada', async () =>{
         const user = await createUserForTest('user1');
         const community = await createCommunityForTest(user!, 'comunidade1');
         const res = await request('http://web-test:6060')
         .get(`/api/c`);
+        expect(res.status).toEqual(200);
+    });
+});
+
+describe('teste da rota /api/c/[comunidade]', () =>{
+    it('Deve retornar 400 caso o input da requisição seja inválido', async () =>{
+        const queryCommunity = 'comunidade teste ;&%'
+        const res = await request('http://web-test:6060')
+        .get(`/api/c/${queryCommunity}`);
+        expect(res.status).toEqual(400);
+    });
+
+    it('Deve retornar 200 caso a requisição seja bem sucedida', async () =>{
+        const queryCommunity = 'comunidade1'
+        // const user = await createUserForTest('user1');
+        // const community = await createCommunityForTest(user!, 'comunidade1');
+        const res = await request('http://web-test:6060')
+        .get(`/api/c${queryCommunity}`);
         expect(res.status).toEqual(200);
     });
 });
