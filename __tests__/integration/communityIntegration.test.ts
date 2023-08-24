393ea3e46f52b737cd4907b562ec4990406a5eda
Eduardo Henrique Silva Santos <eduardo.h3nriqu3@gmail.com>
2023-08-24 19:33:39 -0300

    fix(correção): Correção no teste da rota /c/[comunidade]

--- __tests__/integration/communityIntegration.test.ts
+++ __tests__/integration/communityIntegration.test.ts
@@ -5,13 +5,13 @@ import { createCookie } from "../../src/utils/test_util/createCookie";
 import { createUserForTest } from "../../src/utils/test_util/createUserForTest";
 
 describe('teste da rota /api/c', () =>{
-    it('Deve retornar 404 casó não tenha nenhuma comunidade cadastrada', async () =>{
+    it('Should return 404 if there is no registered community.', async () =>{
         const res = await request('http://web-test:6060')
         .get(`/api/c`);
         expect(res.status).toEqual(404);
     });
 
-    it('Deve retornar 200 casó tenha pelo menos 1 comunidade cadastrada', async () =>{
+    it('Should return 200 if there is at least 1 registered community.', async () =>{
         const user = await createUserForTest('user1');
         const community = await createCommunityForTest(user!, 'comunidade1');
         const res = await request('http://web-test:6060')
@@ -21,17 +21,17 @@ describe('teste da rota /api/c', () =>{
 });
 
 describe('teste da rota /api/c/[comunidade]', () =>{
-    it('Deve retornar 400 caso o input da requisição seja inválido', async () =>{
+    it('Should return 400 if the request input is invalid.', async () =>{
         const queryCommunity = 'comunidade teste ;&%'
         const res = await request('http://web-test:6060')
         .get(`/api/c/${queryCommunity}`);
         expect(res.status).toEqual(400);
     });
 
-    it('Deve retornar 200 caso a requisição seja bem sucedida', async () =>{
+    it('Should return 200 if the request is successful.', async () =>{
         const queryCommunity = 'comunidade1'
-        // const user = await createUserForTest('user1');
-        // const community = await createCommunityForTest(user!, 'comunidade1');
+        const user = await createUserForTest('user1');
+        const community = await createCommunityForTest(user!, 'comunidade1');
         const res = await request('http://web-test:6060')
         .get(`/api/c${queryCommunity}`);
         expect(res.status).toEqual(200);
