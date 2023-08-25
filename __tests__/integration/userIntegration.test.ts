c647dab0867efac379c8144e513b434b6039db1d
Eduardo Henrique Silva Santos <eduardo.h3nriqu3@gmail.com>
2023-08-25 09:25:00 -0300

    fix(fix): fix

--- __tests__/integration/userIntegration.test.ts
+++ __tests__/integration/userIntegration.test.ts
@@ -97,7 +97,7 @@ describe('Test for the /api/update-user Route', () =>{
         ])
         .field('name', 'nome %$.')
         .field('email', 'emailUser%%email.com');
-        expect(res.satus).toEqual(400);
+        expect(res.status).toEqual(400);
     });
 
     it('Should return 400 if the request input email is invalid.', async () =>{
